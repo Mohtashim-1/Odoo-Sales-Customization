1f549d3aae29c3465c196fa6fc3b230f7424f3a5
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-10-27 14:34:40 +0000

    chore: dynamic product code added

--- models/product_template.py
+++ models/product_template.py
@@ -1,10 +1,10 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     launch_date = fields.Date(string='Launch Date')
-    product_code = fields.Char(string='Product Code')
+    product_code = fields.Char(string='Product Code',compute='_compute_product_code', store=True)
     hs_code = fields.Char(string='HS Code')
     packaging_detail_id = fields.Many2one('packaging.detail', string="Packaging Detail")
     length = fields.Float(string="Length")
@@ -17,4 +17,14 @@ class ProductTemplate(models.Model):
     fcl_20 = fields.Float(string="40ft FCL HQ")
     fcl_40 = fields.Float(string="20ft FCL HQ")
     shelf_life = fields.Float(string="Shelf Life")
+
+    @api.depends('name')
+    def _compute_product_code(self):
+        for record in self:
+            if record.name:
+                # Split the name by spaces and take the first letter of each word
+                first_letters = ''.join(word[0].upper() for word in record.name.split() if word)
+                record.product_code = first_letters
+            else:
+                record.product_code = ''
     
