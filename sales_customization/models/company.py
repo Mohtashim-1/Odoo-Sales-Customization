4b70fb4e78d14bb654de5b8cf4a5d1024479ec8c
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2025-02-01 03:52:57 +0800

    setting the data

--- sales_customization/models/company.py
+++ sales_customization/models/company.py
@@ -7,3 +7,4 @@ class ResCompany(models.Model):
     image_field_1 = fields.Image("Company Logo 1")
     image_field_2 = fields.Image("Company Logo 2")
     image_field_3 = fields.Image("Sign")
+#    fda_numbers = fields.Char("FDA Numbers")
