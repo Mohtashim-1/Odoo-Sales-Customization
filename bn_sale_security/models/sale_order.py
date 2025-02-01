545d984bb29f6d0e0f0c64a401049d696387eff2
Syed Owais Noor <119563322+osama123098@users.noreply.github.com>
2025-02-01 12:39:52 +0500

    Update sale_order.py

--- bn_sale_security/models/sale_order.py
+++ bn_sale_security/models/sale_order.py
@@ -23,13 +23,22 @@ class Sale(models.Model):
         default='draft')
 
 
+    # @api.model
+    # def search(self, args, offset=0, limit=None, order=None, count=False):
+    #     raise exceptions.ValidationError('Hit')
+
+    #     """Override search to only show approved orders"""
+    #     args.append(('state', '=', 'approved'))  # Add filter for approved orders
+    #     return super(Sale, self).search(args, offset=offset, limit=limit, order=order, count=count)
+
     @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
+    def _update_sale_order_action(self):
         raise exceptions.ValidationError('Hit')
 
-        """Override search to only show approved orders"""
-        args.append(('state', '=', 'approved'))  # Add filter for approved orders
-        return super(Sale, self).search(args, offset=offset, limit=limit, order=order, count=count)
+        """Dynamically update the action to filter only approved sales"""
+        action = self.env.ref('sale.action_orders')
+        if action:
+            action.sudo().write({'domain': "[('state', '=', 'approved')]"})
 
     def _can_be_confirmed(self):
         self.ensure_one()
