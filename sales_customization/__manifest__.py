f6cafdf32a46038663d9aa96adfe3cfe7a8b8498
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2026-07-09 18:37:31 +0000

    chore: update in performa invoice

--- sales_customization/__manifest__.py
+++ sales_customization/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Sales Customization',
-    'version': '2.3.6',
+    'version': '2.3.7',
     'category': 'Sales',
     'summary': 'Extend and enhance the Sales workflow by adding custom fields to the Product Template and Sales Orders for better data capture and reporting.',
     'description': '''
